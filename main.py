33ebe10b1d1cfcb29d25c443381855bfab28799f
ljcucc <ljcucc@gmail.com>
2021-06-09 02:26:30 +0800

    inject eel.js into angular

--- main.py
+++ main.py
@@ -3,4 +3,7 @@ import eel
 # This is how I do it: https://github.com/robinloop/eel-angular-demo/blob/master/main.py
 
 eel.init('public')
-eel.start({'port': 4200})
+eel.start({'port': 4200},suppress_error=True, options={
+    'port': 8000,
+    'host': 'localhost',
+})
