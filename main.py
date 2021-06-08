34c9bb5e376c376da14698a025dde768561c8c7d
ljcucc <ljcucc@gmail.com>
2021-06-09 02:06:35 +0800

    adding ng serve page into eel app

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
 import eel
 
+# This is how I do it: https://github.com/robinloop/eel-angular-demo/blob/master/main.py
+"""
+----------------------------------------------------------------------------------
+  'options' argument deprecated in v1.0.0, see https://github.com/ChrisKnott/Eel
+  To suppress this error, add 'suppress_error=True' to start() call.
+  This option will be removed in future versions
+----------------------------------------------------------------------------------
+
+WTF?!
+"""
+
 eel.init('public')
-eel.start('index.html')
+eel.start({'port': 4200},suppress_error=True, options={
+  'chromeFlags': ['--auto-open-devtools-for-tabs']
+})
