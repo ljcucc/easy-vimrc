e61767578dcafda01d2d5dc66082e9aad1a7f2e8
ljcucc <ljcucc@gmail.com>
2021-06-09 02:03:38 +0800

    Revert "add something"
    This reverts commit d94d25ff4ccb5b1be9327f27dc3ff71a98574e12.

--- main.py
+++ main.py
@@ -1,15 +1,4 @@
 import eel
 
-mypage = {
-  'scheme': 'http',
-  'host': 'localhost',
-  'port': 4200,
-  'page': 'index.html'
-}
-
 eel.init('public')
-eel.start({'port': 4200}, options={
-  'port': 8000,
-  'host': 'localhost',
-  'chromeFlags': ['--auto-open-devtools-for-tabs']
-})
+eel.start('index.html')
